1003cf09a32c6f662ec9395ae18a3b72b809280f
chanjam03 <jchandler@firstderivatives.com>
2023-01-06 17:01:53 -0500

    Add feed start.sh command

--- bin/start.sh
+++ bin/start.sh
@@ -11,6 +11,7 @@ case $PROCESS in
 	TICK) COMM="${QEXEC} ${PROC_TICK} sym ${DIR_LOG} -p ${PORT_TICK} -dirHome ${DIR_HOME}";;
 	RDB1) COMM="${QEXEC} ${PROC_RDB} -p ${PORT_RDB1} -tp ${PORT_TICK} -hdb ${PORT_HDB} -dirHome ${DIR_HOME} -dirHDB ${DIR_HDB} -sub trade quote";;
 	RDB2) COMM="${QEXEC} ${PROC_RDB} -p ${PORT_RDB2} -tp ${PORT_TICK} -hdb ${PORT_HDB} -dirHome ${DIR_HOME} -dirHDB ${DIR_HDB} -sub OHLC";;
+	FEED) COMM="${QEXEC} ${PROC_FEED} -p ${PORT_FEED} -tp ${PORT_TICK}";;
 esac
 
 $COMM
