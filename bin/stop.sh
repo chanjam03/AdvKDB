da7e87eda5f42b29e59aa3f09d628ab4728bcff1
Jamie Chandler <jchandler@firstderivatives.comgit config --global user.name Jamie>
2023-04-18 15:57:07 -0400

    Hot fix to reverse stop order

--- bin/stop.sh
+++ bin/stop.sh
@@ -26,17 +26,17 @@ PROCESS=$1
 # Stopping process
 echo ""
 case $PROCESS in
-    ALL)
-        echo "Stopping tick process"
-        lsof -i :${PORT_TICK} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
+        ALL)
+        echo "Stopping realtime engine process"
+        lsof -i :${PORT_RTE} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
+        echo "Stopping feed process"
+        lsof -i :${PORT_FEED} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
         echo "Stopping rdb1 process"
         lsof -i :${PORT_RDB1} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
         echo "Stopping rdb2 process"
         lsof -i :${PORT_RDB2} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
-        echo "Stopping feed process"
-        lsof -i :${PORT_FEED} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9
-        echo "Stopping realtime engine process"
-        lsof -i :${PORT_RTE} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9;;
+        echo "Stopping tick process"
+        lsof -i :${PORT_TICK} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9;;
 	TICK)
         echo "Stopping tick process"
         lsof -i :${PORT_TICK} | grep LISTEN | awk '{print $2; exit}' | xargs kill -9;;
