5a4c7c8d8447c37434985471623926d626c381d7
Jamie Chandler <jchandler@firstderivatives.comgit config --global user.name Jamie>
2023-04-23 23:30:24 -0400

    Add eod saving down process

--- bin/eod.sh
+++ bin/eod.sh
@@ -5,18 +5,37 @@
 ##  CSV file shell start process for python or java (q called with normal start 
 ##  script)
 ##
-##  sh load.sh <LANGUAGE> <NAME_CSV> <NAME_TABLE>
+##  sh eod.sh 
 ##
-##  i.e. sh load.sh PYTHON optTrades.csv optTrades
-##
-##  Parameters:
-##      - [LANGUAGE]: script to call the csv load process
-##      - [NAME_CSV]: filename of csv to load in and send to tickerplant (All 
-##          files should be placed in files directory)
-##      - [NAME_TABLE]: name of table to be updated on tickerplant (should be 
-##          added to sym schema file)
-##
-##  Options:
-##      - [LANGUAGE] -> "Q","PYTHON","JAVA"
+##  i.e. sh eod.sh
 ##
 ################################################################################
+
+# Sourcing config profile
+source ./config/config.profile
+
+# Reading in optional tags
+interactive=false
+while getopts :i opt; do
+    case $opt in 
+        i) interactive=true ;;
+       \?) echo "Unknown option -$OPTARG"; exit 1;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+PROCESS=$1
+
+# Defining start commands
+if $interactive; then
+    START_EOD="${QEXEC} ${PROC_EOD} -p ${PORT_EOD} -tp ${PORT_TICK} -libCommon ${LIB_COMMON} -libSym ${LIB_SYM} -dirReplay ${DIR_LOGS_REP} -dirHDB ${DIR_HDB}"
+else
+    START_EOD="${QEXEC} ${PROC_EOD} -p ${PORT_EOD} -tp ${PORT_TICK} -libCommon ${LIB_COMMON} -libSym ${LIB_SYM} -dirReplay ${DIR_LOGS_REP} -dirHDB ${DIR_HDB} </dev/null >> ${DIR_LOGS_PROC}/eod.log 2>&1 &"
+fi
+
+# Starting process
+echo ""
+echo "Starting end of day process"
+eval $START_EOD
+echo "End of day process complete"
+echo ""
--- q/eod.q
+++ q/eod.q
@@ -12,3 +12,26 @@
 //      - [libCommon]: common library containing functions
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+// Parsing command line arguments and sourcing applicable libraries
+args:(.Q.opt .z.x);
+system raze "l ",(args `libCommon);
+system raze "l ",(args `libSym);
+dirHDB:args `dirHDB;
+tpLog:hsym first `$ raze (args `dirReplay),"/sym",(string .z.D);
+
+// Defining update function and saving function for parsing replayed log file
+upd:{[tab;data] insert[tab;data]}
+
+saveTab:{[dt;tab]
+    fp:hsym `$ raze dirHDB,"/",(string dt),"/",(string tab),"/";
+    compCols:(),cols[tab] except `time`sym;
+    compDets:compCols!(count compCols)#enlist (17;2;6);
+    (fp;compDets) set .Q.en[hsym first `$ dirHDB;get tab]
+    };
+
+// Replaying log file and updating tables in memory
+-11!tpLog;
+
+// Saving down each table with compression on all other cols
+saveTab[.z.D] each tables[`.];
